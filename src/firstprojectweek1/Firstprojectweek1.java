33bb6411883812ed46e98fb1a1af0132503cd2c5
Komalpreet Kaur <kau15367@sheridancollege.ca>
2024-01-17 09:45:07 -0500

    First commit

---
+++ src/firstprojectweek1/Firstprojectweek1.java
@@ -0,0 +1,33 @@
+
+package firstprojectweek1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class Firstprojectweek1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter the word");
+        Scanner input = new Scanner(System.in);
+        String myWord = input.nextLine();
+        
+        char[] myLetters = new char[myWord.length()];
+        for(int i=0;i<myLetters.length;i++){
+            myLetters[i] = myWord.charAt(i);
+        }
+        System.out.println("Printing in reverse");
+        for(int i=myLetters.length-1;i>=0;i--){
+            System.out.println(myLetters[i]);
+        }
+        // TODO code application logic here
+        //System.out.println(myWord);
+        
+    }
+    
+}
